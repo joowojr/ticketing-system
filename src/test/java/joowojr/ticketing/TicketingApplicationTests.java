f1caa4e38687554be763f9bb8b58f9003f8c94d5
joowojr <joowojr@gmail.com>
2025-02-26 16:08:27 +0900

    🎉 Initial Commit

---
+++ src/test/java/joowojr/ticketing/TicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package joowojr.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
